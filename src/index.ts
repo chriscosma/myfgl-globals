c8cc2abe1a99a7bcaf596f4beedb8e99f9f58905
Jordan Kula <55468681+jkula25@users.noreply.github.com>
2024-04-21 13:28:52 -0400

    Correct championship week globals to end of week

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 export const globals = {
   CONFERENCE_DICT: {
     week: 14,
-    date: '2024-04-01'
+    date: '2024-04-07'
   },
   CHAMPIONSHIP_DICT: {
     week: 16,
-    date: '2024-04-15'
+    date: '2024-04-21'
   }
 }
